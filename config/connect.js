8d1f9cdb8b244be6f88ad6aa5072f815a95ead23
Chris <Christopherjorge47@icloud.com>
2024-04-27 21:12:27 -0400

    Added connection code

---
+++ config/connect.js
@@ -0,0 +1,10 @@
+requestAnimationFrame("dotenv").config();
+
+const mongoose = require('mongoose');
+
+const connectToDataBase = async() => {
+    await mongoose.connect(process.env.CONNECTIONSTR);
+    console.log("Currently Connected To MongoDB Cluster");
+}
+
+module.exports = connectToDataBase
